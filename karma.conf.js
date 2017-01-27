7a31381e0062d7c7fdcafefb2a121d13ea787b3d
Aiham Hammami <aiham@aiham.net>
2017-01-27 13:37:58 +1100

    Add karma config

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = config => {
+  config.set({
+    basePath: '',
+    frameworks: ['browserify', 'jasmine'],
+    files: [
+      'test/**/*.js'
+    ],
+    preprocessors: {
+      'src/**/*.js': ['babel', 'browserify'],
+      'test/**/*.js': ['babel', 'browserify']
+    },
+    browserify: {
+      debug: true,
+      transform: [
+        ['babelify']
+      ],
+      configure: bundle => {
+        bundle.on('prebundle', () => {
+          bundle.external('react/addons');
+          bundle.external('react/lib/ReactContext');
+          bundle.external('react/lib/ExecutionEnvironment');
+        });
+      }
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false
+  });
+};
