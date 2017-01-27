653d7afcd18860299902024f82f65a0794b1bfad
Aiham Hammami <aiham@aiham.net>
2017-01-27 13:37:37 +1100

    Add class names for elements created by OTSubscriber

--- src/OTSubscriber.js
+++ src/OTSubscriber.js
@@ -18,6 +18,7 @@ export default class OTSubscriber extends Component {
     }
 
     let container = document.createElement('div');
+    container.setAttribute('class', 'OTSubscriberContainer');
     findDOMNode(this).appendChild(container);
 
     let subscriber = this.props.session.subscribe(
@@ -104,7 +105,7 @@ export default class OTSubscriber extends Component {
   }
 
   render() {
-    return <div />;
+    return <div className='OTSubscriber' />;
   }
 }
 
