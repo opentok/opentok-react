71606b44bf377870e1fba36167d748c3809ca06a
Enrico Portolan <eportolan@VONFVFYJ0ELHV2G.local>
2019-06-07 17:21:33 +0100

    - add Republish button in the example page - add Resubscribe button in the example page

--- example/components/Publisher.js
+++ example/components/Publisher.js
@@ -5,70 +5,79 @@ import RadioButtons from './RadioButtons';
 import CheckBox from './CheckBox';
 
 export default class Publisher extends Component {
-  constructor(props) {
-    super(props);
+    constructor( props ) {
+        super( props );
 
-    this.state = {
-      error: null,
-      audio: true,
-      video: true,
-      videoSource: 'camera'
-    };
-  }
+        this.state = {
+            error: null,
+            audio: true,
+            video: true,
+            videoSource: 'camera'
+        };
+        this.otPublisher = null;
+    }
 
-  setAudio = (audio) => {
-    this.setState({ audio });
-  }
+    setAudio = ( audio ) => {
+        this.setState( { audio } );
+    }
 
-  setVideo = (video) => {
-    this.setState({ video });
-  }
+    setVideo = ( video ) => {
+        this.setState( { video } );
+    }
 
-  setVideoSource = (videoSource) => {
-    this.setState({ videoSource });
-  }
+    setVideoSource = ( videoSource ) => {
+        this.setState( { videoSource } );
+    }
 
-  onError = (err) => {
-    this.setState({ error: `Failed to publish: ${err.message}` });
-  }
+    onError = ( err ) => {
+        this.setState( { error: `Failed to publish: ${err.message}` } );
+    }
 
-  render() {
-    return (
-      <div>
-        {this.state.error ? <div>{this.state.error}</div> : null}
-        <OTPublisher
-          properties={{
-            publishAudio: this.state.audio,
-            publishVideo: this.state.video,
-            videoSource: this.state.videoSource === 'screen' ? 'screen' : undefined
-          }}
-          onError={this.onError}
-        />
-        <RadioButtons
-          buttons={[
-            {
-              label: 'Camera',
-              value: 'camera'
-            },
-            {
-              label: 'Screen',
-              value: 'screen'
-            }
-          ]}
-          initialChecked={this.state.videoSource}
-          onChange={this.setVideoSource}
-        />
-        <CheckBox
-          label="Publish Audio"
-          initialChecked={this.state.audio}
-          onChange={this.setAudio}
-        />
-        <CheckBox
-          label="Publish Video"
-          initialChecked={this.state.video}
-          onChange={this.setVideo}
-        />
-      </div>
-    );
-  }
+    rePublish = () => {
+        this.otPublisher.destroyPublisher();
+        this.otPublisher.createPublisher();
+    }
+
+    render () {
+        return (
+            <div>
+                {this.state.error ? <div>{this.state.error}</div> : null}
+                <OTPublisher
+                    properties={{
+                        publishAudio: this.state.audio,
+                        publishVideo: this.state.video,
+                        videoSource: this.state.videoSource === 'screen' ? 'screen' : undefined
+                    }}
+                    onError={this.onError}
+                    eventHandlers={this.props.eventHandlers}
+                    ref={(instance) => { this.otPublisher = instance }}
+                />
+                <RadioButtons
+                    buttons={[
+                        {
+                            label: 'Camera',
+                            value: 'camera'
+                        },
+                        {
+                            label: 'Screen',
+                            value: 'screen'
+                        }
+                    ]}
+                    initialChecked={this.state.videoSource}
+                    onChange={this.setVideoSource}
+                />
+                <CheckBox
+                    label="Publish Audio"
+                    initialChecked={this.state.audio}
+                    onChange={this.setAudio}
+                />
+                <CheckBox
+                    label="Publish Video"
+                    initialChecked={this.state.video}
+                    onChange={this.setVideo}
+                />
+                <button onClick={this.rePublish} > Republish </button>
+            </div>
+        );
+    }
 }
